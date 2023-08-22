e82f7d45242dc58002e4c1b0ccb3b3f67c56b404
rajan <rajanshahi1267@gmail.com>
2023-08-22 00:29:28 -0700

    first init code

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./home.css"
+import Navbar from '../../components/navbar/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Home
