300a700111cef347eb8d8b9610cbc852685eb711
rajan <rajanshahi1267@gmail.com>
2023-08-22 01:34:54 -0700

    add navbar components

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "./header.css";
+import { BiSolidBed } from "react-icons/bi";
+import { MdFlightTakeoff } from "react-icons/md";
+import { AiFillCar } from "react-icons/ai";
+import { GrAttraction } from "react-icons/gr";
+import { FaTaxi } from "react-icons/fa";
+import { AiFillCalendar } from "react-icons/ai";
+import { BsFillPersonFill } from "react-icons/bs";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <BiSolidBed />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <MdFlightTakeoff />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <AiFillCar />
+            <span>Cars rentals</span>
+          </div>
+          <div className="headerListItem">
+            <GrAttraction />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FaTaxi />
+            <span>Airport taxis</span>
+          </div>
+        </div>
+        <h1 className="headerTitle">A Lifetime of discounts? It's Genius.</h1>
+        <p className="headerDesc">
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button className="headerBtn">Sign in / Register</button>
+        <div className="headerSearch">
+          <div className="headerIcon">
+            <BiSolidBed />
+            <input
+              type="text"
+              placeholder="Where are you going?"
+              className="headerSearchInput"
+            />
+          </div>
+
+          <div className="headerIcon">
+            <AiFillCalendar />
+           <span className="headerSearchText">date to date</span>
+          </div>
+
+          <div className="headerIcon">
+            <BsFillPersonFill />
+            <span className="headerSearchText">2 adults 2 children 1 room</span>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
