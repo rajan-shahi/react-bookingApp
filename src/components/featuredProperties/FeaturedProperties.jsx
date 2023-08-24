ea65145d299c4880ef3ecedc86040c5abbc7490e
rajan <rajanshahi1267@gmail.com>
2023-08-23 23:08:15 -0700

    add productList components

---
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./featuredProperties.css"
+
+const FeaturedProperties = () => {
+  return (
+   <div className="fp">
+    
+   </div>
+  )
+}
+
+export default FeaturedProperties
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import "./home.css"
 import Navbar from '../../components/navbar/Navbar'
 import Header from '../../components/header/Header'
 import Featured from '../../components/featured/Featured'
+import PropertyList from '../../components/propertyList/PropertyList'
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties'
 
 const Home = () => {
   return (
@@ -14,6 +16,9 @@ const Home = () => {
         <h1 className="homeTitle">
           Browse by property type
         </h1>
+        <PropertyList/>
+        <h1 className="homeTitle">Home guests love</h1>
+        <FeaturedProperties/>
       </div>
     </div>
   )
